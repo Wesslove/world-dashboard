de8092fe8f150c4d1df22920b0cd630b7ae7e4b3
Wess <adhonaikouka@gmail.com>
2026-04-20 13:10:42 +0100

    Update:  Mise à jour du fichier de configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/world-dashboard/'
+  // Utiliser './' rend les chemins relatifs au lieu d'absolus
+  base: './' 
 })
